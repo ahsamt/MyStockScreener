e6e2aae3b7b59f4b84f323f5b43fc6af3c3de84a
Maryia <tarpachova@gmail.com>
2022-02-08 17:56:01 +0000

    initial setup completed

--- MyStockScreener/settings.py
+++ MyStockScreener/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'MyStockScreener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'stock_screener/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- stock_screener/views.py
+++ stock_screener/views.py
@@ -1,3 +1,74 @@
+from django import forms
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import User
+from django.db import IntegrityError
 
-# Create your views here.
+
+class StockForm(forms.Form):
+    stock = forms.CharField(label='Stock name', max_length=5,
+                            widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'AAPL'}))
+
+
+def index(request):
+    if request.method == "GET":
+        return render(request, "stock_screener/index.html", {"stockForm": StockForm()})
+
+    if request.method == "POST":
+        if 'stock' in request.POST:
+            stockForm = StockForm(request.POST)
+            if stockForm.is_valid():
+                stock = stockForm.cleaned_data['stock'].upper()
+
+        return render(request, "stock_screener/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication is successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "stock_screener/login.html", {"message": "Invalid username and/or password"})
+    else:
+        return render(request, "stock_screener/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Make sure password matches password confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "stock_screener/register.html", {
+                "message": "Please make sure the passwords match"
+            })
+
+        # Attempt to create a new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "stock_screener/register.html", {
+                "message": "Sorry, this username is not available"
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "stock_screener/register.html")
