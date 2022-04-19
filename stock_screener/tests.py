594fc8c27f795a97c104f16c3c9e5cf9322018a7
Maryia <tarpachova@gmail.com>
2022-04-19 17:27:13 +0100

    registration added to selenium test setup, backtesting selenium test to be finalised

--- stock_screener/tests.py
+++ stock_screener/tests.py
@@ -63,15 +63,34 @@ class GeneralTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class SearchTest(LiveServerTestCase):
+class SeleniumTest(LiveServerTestCase):
+    signals = ['ma', 'psar', 'adx', 'srsi', 'macd']
+    num_of_signals = range(1, (len(signals) + 1))
+    username = "tester"
+    email = "tester@tester.tester"
+    password = "tester"
+    confirmation = "tester"
 
     def setUp(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(self.live_server_url)
+        login_link = self.driver.find_element_by_link_text("Register")
+        self.driver.execute_script("arguments[0].click();", login_link)
+
+        username_el = self.driver.find_element_by_name("username")
+        username_el.send_keys(self.username)
+        email_el = self.driver.find_element_by_name("email")
+        email_el.send_keys(self.email)
+        password_el = self.driver.find_element_by_name("password")
+        password_el.send_keys(self.password)
+        confirmation_el = self.driver.find_element_by_name("confirmation")
+        confirmation_el.send_keys(self.password)
+
+        self.driver.find_element_by_css_selector("form").submit()
 
     def test_search_no_signal(self):
-        element = self.driver.find_element_by_id('searchButton')
-        self.driver.execute_script("arguments[0].click();", element)
+        search_link = self.driver.find_element_by_link_text("Search")
+        self.driver.execute_script("arguments[0].click();", search_link)
         self.driver.find_element_by_id('id_ticker').send_keys("AAPL")
         element = self.driver.find_element_by_id('searchButton')
         self.driver.execute_script("arguments[0].click();", element)
@@ -79,19 +98,18 @@ class SearchTest(LiveServerTestCase):
         graph = self.driver.find_element_by_class_name('plotly-graph-div')
         assert resultTable is not None
         assert graph is not None
+        #add_link = self.driver.find_element_by_class_name('watchlist')
+        #self.driver.execute_script("arguments[0].click();", add_link)
 
 
     def test_search_with_signals(self):
-        signals = ['ma', 'psar', 'adx', 'srsi', 'macd']
-        num_of_signals = range(1, (len(signals) + 1))
-        element = self.driver.find_element_by_id('searchButton')
-        self.driver.execute_script("arguments[0].click();", element)
-        for n in num_of_signals:
+
+        for n in self.num_of_signals:
             search_link = self.driver.find_element_by_link_text("Search")
             self.driver.execute_script("arguments[0].click();", search_link)
 
-            self.driver.find_element_by_id('id_ticker').send_keys("AAPL")
-            selected_signals = random.sample(signals, k=n)
+            self.driver.find_element_by_id('id_ticker').send_keys("GS")
+            selected_signals = random.sample(self.signals, k=n)
             print(n, selected_signals)
             for s in selected_signals:
                 s_element = self.driver.find_element_by_id(f"id_{s}")
@@ -104,6 +122,27 @@ class SearchTest(LiveServerTestCase):
             graph = self.driver.find_element_by_class_name('plotly-graph-div')
             assert resultTable is not None
             assert graph is not None
+
+    def test_backtest_with_signals(self):
+
+
+        for n in self.num_of_signals:
+            backtest_link = self.driver.find_element_by_link_text("Back Tester")
+            self.driver.execute_script("arguments[0].click();", backtest_link)
+
+            selected_signals = random.sample(self.signals, k=n)
+            print(n, selected_signals)
+            for s in selected_signals:
+                s_element = self.driver.find_element_by_id(f"id_{s}")
+                if not s_element.is_selected():
+                    self.driver.execute_script("arguments[0].click();", s_element)
+
+            element = self.driver.find_element_by_id('searchButton')
+            self.driver.execute_script("arguments[0].click();", element)
+            resultTable = self.driver.find_element_by_class_name('outcome_card')
+
+            assert resultTable is not None
+
 
     def tearDown(self):
         self.driver.quit
